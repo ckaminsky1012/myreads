68df533a2a713be02f745781e3d3dbbe471d3cdb
Corey Kaminsky <ckaminsky@fb.com>
2017-07-15 19:08:37 -0400

    updated style to individual bookpage

--- src/Book.js
+++ src/Book.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as BooksAPI from './BooksAPI'
+import { Link } from 'react-router-dom'
 
 class Book extends React.Component {
 
@@ -17,28 +18,34 @@ class Book extends React.Component {
     })
   }
 
-
   render() {
 
     const { book } = this.state
-    console.log(this.state.book.imageLinks)
+    console.log(this.state.book)
 
     return (
-
-      <div className="bookshelf">
-      <div className="book-cover" style={{ width: 300, height: 396, backgroundImage: `url("${book.imageLinks && book.imageLinks.smallThumbnail}")` }}>
-                  </div>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            <li>
-              <div className="book">
-                <div className="book-top">
-                  <div className="book-title">{book.title}</div>
-                  <div className="book-authors">{book.authors}</div>
-                </div>
+      <div className="app">
+        <Link className="close-search" to="/">Close</Link>
+        <div className="container" style={{width: 970}}>
+          <div className="row">
+            <div className="col-md-3">
+              <div className="book-cover-new" style={{ width: 181, height: 284,  backgroundImage: `url("${book.imageLinks && book.imageLinks.smallThumbnail}")` }}>
               </div>
-            </li>
-          </ol>
+            </div>
+            <div className="col-md-7">
+              <div className="indv-book-title">{book.title}</div>
+              <div className="indv-book-subtitle">{book.subtitle}</div>
+              <div className="indv-book-authors">By: {book.authors && book.authors.join(', ')}</div>
+              <div className="indv-book-details">
+              <br/>
+                <p> {book.description} </p>
+                <p> <b>Publisher:</b> {book.publisher} </p>
+                <p> <b>Published On:</b> {book.publishedDate} </p>
+                <p> <b>Page Count:</b> {book.pageCount} </p>
+                <p> <b>Category:</b> {book.categories} </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -41,34 +41,31 @@ class SearchPage extends React.Component {
 			            	/>
 			            </div>
 			    </div>
-			        <div className="search-books-results">
-			            <ol className="books-grid">
-			                {showingBooks.map((book)=>(
-                   <li key={book.id} >
-                   <div className="book">
-                    <div className="book-top">
-                     <div className="book-cover" style={{ width: 128, height: 193,
-                      backgroundImage:`url("${book.imageLinks.thumbnail}")` }}>
-                                </div>
-                      <div className="book-shelf-changer">
-                              <select value={book.shelf} onChange={event =>onUpdateShelf(book, event.target.value)}>
-                                <option value="none" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                             </div>
-                           </div>
-                          <div className="book-title">{book.title}</div>
-                          <div className="book-authors">{book.authors}</div>
-
-                      </div>
-
-                   </li>
-                      ))}
-			    </ol>
-			        </div>
+			    <div className="search-books-results">
+			        <ol className="books-grid"> {showingBooks.map((book)=>(
+                   		<li key={book.id} >
+                   			<div className="book">
+                    			<div className="book-top">
+                    				<Link to={`/book/${book.id}`}>
+	                     				<div className="book-cover" style={{ width: 128, height: 193, backgroundImage:`url("${book.imageLinks.thumbnail}")` }}>
+	                                	</div>
+	                                </Link>
+                      				<div className="book-shelf-changer">
+		                            	<select value={book.shelf} onChange={event =>onUpdateShelf(book, event.target.value)}>
+		                                	<option value="none" disabled>Move to...</option>
+		                                	<option value="currentlyReading">Currently Reading</option>
+		                                	<option value="wantToRead">Want to Read</option>
+		                                	<option value="read">Read</option>
+		                                	<option value="none">None</option>
+		                            	</select>
+                             		</div>
+                           		</div>
+                          	<div className="book-title">{book.title}</div>
+                          	<div className="book-authors">{book.authors}</div>
+                      		</div>
+                   		</li>
+                    ))} </ol>
+			    </div>
 			</div>
 		)
 	}
